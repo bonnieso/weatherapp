d4a77b8e3654ba9925a81010f532d0150e834b48
bonnieso <so_bonnie@hotmail.com>
2015-07-17 01:11:53 -0700

    adding change to app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require("express-session");
 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
